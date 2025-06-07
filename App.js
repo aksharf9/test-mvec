ffafadfd6fc647cb28cd09f1d963f4371c953a99
aksharf9 <ak.sharf9@gmail.com>
2025-06-07 20:44:00 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,59 +1,55 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import StoresPage from './pages/StoresPage';
-import CategoryPage from './pages/CategoryPage';
-import AboutPage from './pages/AboutPage';
+  import React from "react";
+  import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+  import HomePage from './pages/HomePage';
+  import LoginPage from './pages/LoginPage';
+  import RegisterPage from './pages/RegisterPage';
+  import StoresPage from './pages/StoresPage';
+  import CategoryPage from './pages/CategoryPage';
+  import AboutPage from './pages/AboutPage';
+  import AppAdmin from './AppAdmin';
+  import AppVendor from './AppVendor';
 
-import AppAdmin from './AppAdmin';
-import AppVendor from './AppVendor';
+  // Import the new guard
+  import PrivateAdminRoute from "./components/PrivateAdminRoute";
+  // Optional: import PrivateVendorRoute if you want to protect vendor as well
+  // import PrivateVendorRoute from "./components/PrivateVendorRoute";
 
 
-import AdminLogin from './pages/Admin/AdminLogin';
 
 
+  function App() {
+    return (
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/stores-near-you" element={<StoresPage />} />
+          <Route path="/categories" element={<CategoryPage />} />
+          <Route path="/about" element={<AboutPage />} />
 
-// Import the new guard
-import PrivateAdminRoute from "./components/PrivateAdminRoute";
-// Optional: import PrivateVendorRoute if you want to protect vendor as well
-// import PrivateVendorRoute from "./components/PrivateVendorRoute";
+          {/* Protected Admin Routes */}
+          <Route path="/admin/*" element={
+            <PrivateAdminRoute>
+              <AppAdmin/>
+            </PrivateAdminRoute>
+          } />
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/stores-near-you" element={<StoresPage />} />
-        <Route path="/categories" element={<CategoryPage />} />
-        <Route path="/about" element={<AboutPage />} />
-<Route path="/Admin/Login" element={<AdminLogin />} />
+          {/* Protected Vendor Routes - optional */}
+          {/*}
+          <Route path="/vendor/*" element={
+            <PrivateVendorRoute>
+              <AppVendor />
+            </PrivateVendorRoute>
+          } />
+          
 
-        {/* Protected Admin Routes */}
-        <Route path="/admin/*" element={
-          <PrivateAdminRoute>
-            <AppAdmin />
-          </PrivateAdminRoute>
-        } />
+          {/* Unprotected Vendor Routes (remove if protecting) */}
+          <Route path="/vendor/*" element={<AppVendor />} />
+        </Routes>
+      </Router>
+    );
+  }
 
-        {/* Protected Vendor Routes - optional */}
-        {/* 
-        <Route path="/vendor/*" element={
-          <PrivateVendorRoute>
-            <AppVendor />
-          </PrivateVendorRoute>
-        } />
-        */}
-
-        {/* Unprotected Vendor Routes (remove if protecting) */}
-        <Route path="/vendor/*" element={<AppVendor />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+  export default App;
